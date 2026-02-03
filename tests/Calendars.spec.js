baf71223f15f374707830af39fd0b2d0abb1ddf3
Junaid Malik <junaidmalik0971@gmail.com>
2026-02-03 15:23:55 +0500

    Popups and Calendar Validation

--- tests/Calendars.spec.js
+++ tests/Calendars.spec.js
@@ -5,6 +5,7 @@ test('Calendar Validations', async ({page}) => {
     const monthNumber = "6";
     const date = "15";
     const year = "2027";
+    const expectedList = [monthNumber, date, year];
 
     await page.goto("https://rahulshettyacademy.com/seleniumPractise/#/offers");
     await page.locator(".react-date-picker__inputGroup").click();   // click on date picker input field to open calendar
@@ -13,6 +14,13 @@ test('Calendar Validations', async ({page}) => {
     await page.getByText(year).click();   // select the desired year
     await page.locator(".react-calendar__year-view__months__month").nth(parseInt(monthNumber) - 1).click();  // select the desired month
     await page.locator("//abbr[text()='"+date+"']").click();  // select the desired date
+    const inputs = page.locator(".react-date-picker__inputGroup__input");
+
+    for (let i = 0; i<expectedList.length; ++i){
+        const value = await inputs.nth(i).inputValue();
+        expect(value).toEqual(expectedList[i]);
+        console.log(`Assertion passed for ${i} iteration`);
+    }
 
 })
 
---
+++ tests/moreValidations.spec.js
@@ -0,0 +1,13 @@
+const {test, expect} = require('@playwright/test');
+const { only } = require('node:test');
+
+test("Popup Validations", async ({page}) => {
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    // await page.goto("https://google.com");
+    // await page.goBack();
+    // await page.goForward();
+
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    })
